d8393bcdd6e2a56993b400fed3d1058eac2115ab
valeria <167262008+vale-rg02@users.noreply.github.com>
2026-02-05 15:47:25 -0700

    Add files via upload

---
+++ SistemaVentas3/SistemaVentas/CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string Conn = "Data source = LAPTOP-36HHFTU4\\SQLEXPRESS; Initial Catalog = dbventas; Integrated Security = true";
+    }
+}
---
+++ SistemaVentas3/SistemaVentas/CapaNegocio/CNCliente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using CapaDatos;
+using System.Net.NetworkInformation;
+
+namespace CapaNegocio
+{
+    public class CNCliente
+    {
+        public static DataTable Listar()
+        {
+            CDCliente Datos = new CDCliente();
+            return Datos.Listar();
+        }
+
+        public static string Guardar(string nombre, string apellidos, string rfc, string telefono, string estado)
+        {
+            CDCliente Datos = new CDCliente();
+            Datos.Nombre = nombre;
+            Datos.Apellidos = apellidos;
+            Datos.Rfc = rfc;
+            Datos.Telefono = telefono;
+            Datos.Estado = estado;
+            return Datos.Guardar(Datos);
+        }
+
+        public static string Editar(int idcliente, string nombre, string apellidos, string rfc, string telefono, string estado)
+        {
+            CDCliente Datos = new CDCliente();
+            Datos.IdCliente = idcliente;
+            Datos.Nombre = nombre;
+            Datos.Apellidos = apellidos;
+            Datos.Rfc = rfc;
+            Datos.Telefono = telefono;
+            Datos.Estado = estado;
+            return Datos.Editar(Datos);
+        }
+
+        public static string Eliminar(int idcliente)
+        {
+            CDCliente Datos = new CDCliente();
+            Datos.IdCliente = idcliente;
+            return Datos.Eliminar(Datos);
+        }
+
+        public static DataTable BuscarNombre(string textobuscar)
+        {
+            CDCliente Datos = new CDCliente();
+            Datos.Buscar = textobuscar;
+            return Datos.BuscarNombre(Datos);
+        }
+
+        public static DataTable BuscarDni(string textobuscar)
+        {
+            CDCliente Datos = new CDCliente();
+            Datos.Buscar = textobuscar;
+            return Datos.BuscarDni(Datos);
+        }
+    }
+}
